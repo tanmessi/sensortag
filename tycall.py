cad91fea20ede2e11b75c606111c0caa4c3d0646
TyAiTi <35554335+TyAiTi@users.noreply.github.com>
2019-11-29 14:41:18 +0700

    goi dien dung python 2
    gọi điện dùng python2

---
+++ tycall.py
@@ -0,0 +1,14 @@
+#goi dien thoai
+import RPi.GPIO as GPIO
+import serial 
+import time,sys
+
+SERIAL_PORT = "/dev/ttyS0"
+ser = serial.Serial(SERIAL_PORT,baudrate = 9600, timeout=5)
+
+ser.write("ATD+84978912207;\r")
+print("Dialing ...")
+time.sleep(10) //goi 10s
+ser.write("ATH\r")
+print("Hanging up ...")
+
