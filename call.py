7c6470b6400b0333864c76972704bc61742a54a3
TyAiTi <35554335+TyAiTi@users.noreply.github.com>
2019-12-03 23:47:12 +0700

    update root

--- call.py
+++ call.py
@@ -3,18 +3,24 @@ import RPi.GPIO as GPIO
 import serial 
 #from serial import Serial
 import time,sys
-import sdt
-sdt=sdt.a
+import numphone
+numphone=numphone.a
 chuoi=";\r"
 SERIAL_PORT = "/dev/ttyS0"
 ser = serial.Serial(SERIAL_PORT,baudrate = 9600, timeout=5)
 
-ser.write(("ATD"+(sdt)+(chuoi)).encode())
+ser.write(("ATD"+(numphone)+(chuoi)).encode())
 print("Dialing ...")
 time.sleep(15)
 ser.write("ATH\r".encode())
 print("Hanging up ...")
 
-print("Ban co muon thay doi so dien thoai")
-import write
-print("So dien thoai da thay doi")
+print("Ban co muon thay doi so dien thoai hay nhap 1, neu khong nhap 0")
+a = 0
+a = input()
+if a == "1" :
+  import write
+  print("So dien thoai da thay doi")
+else:
+  print("Khong thay doi")
+
